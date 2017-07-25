8e61eff47ea662a5d8cded442ef4ac42f919468a
Kaveh Mousavi Zamani <k.mousavizamani@travelaudience.com>
2017-07-25 17:09:02 +0200

    adding basic New and Start command with no details

--- example/main.go
+++ example/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "github.com/travelaudience/sqlbench"
+import "fmt"
+import "log"
 
+func main() {
+	b, e := sqlbench.New("example/config.json")
+	if e != nil {
+		log.Fatal(e)
+	}
+	w := b.Start()
+	<-w
+	fmt.Println("Finished")
 }
--- sqlbench.go
+++ sqlbench.go
@@ -8,7 +8,7 @@ type Config struct {
 	Xtags []struct {
 		// Name can be any string
 		Name string `json:"name"`
-		// Value can be only a query which will result in a numeric value. Alternatively it can be `epoch` or `datetime`.
+		// Value can be only a query which will result in a numeric value. Alternatively it can be `epoch` or `datetime`
 		Value string `json:"value"`
 	} `json:"xtags"`
 	Queries []struct {
@@ -18,19 +18,43 @@ type Config struct {
 		Frequency int `json:"1"`
 		// Number of parallel runs for this query
 		Parallel int `json:"2"`
+		// Number if runs for this query
+		Count int `json:"2"`
 		// Query to run
 		Query string `json:"3"`
 	} `json:"queries"`
 	// PostgreSQL database DSN
 	Db   string `json:"db"`
 	Logs struct {
-		// If set it will append the logs to this csv file.
+		// If set it will append the logs to this csv file
 		Csv string `json:"csv"`
 		// If set it will send the results to datadog
 		Datadog string `json:"datadog"`
 	} `json:"logs"`
 }
 
-func pick() {
+// Bench is the structure which encapsulated the required methods for running benchmark.
+type Bench struct {
+	config Config
+	wait   chan bool
+}
 
+// New will return a Bench structure that can be used to control the benchmark.
+func New(configFile string) (Bench, error) {
+	b := Bench{}
+	var err error
+	if b.config, err = config(configFile); err != nil {
+		return b, err
+	}
+	return b, nil
+}
+
+// Start will started the benchmark and immediately returns.
+// The returned channel can be used for waiting until benchmark is finish.
+func (b *Bench) Start() chan bool {
+	b.wait = make(chan bool)
+
+	go b.start()
+
+	return b.wait
 }
